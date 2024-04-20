a602a15dfddefd02856bcbe47132d16a916614f5
Jonathan Larsen <jonny@jlarsen.io>
2024-04-20 09:31:29 -0500

    Update pipe.ts

--- src/functions/pipe.ts
+++ src/functions/pipe.ts
@@ -4,20 +4,21 @@
 
 type AnyFunc = (...arg: any) => any;
 
-type PipeArgs<F extends AnyFunc[], Acc extends AnyFunc[] = []> = F extends [
-  (...args: infer A) => infer B
-]
-  ? [...Acc, (...args: A) => B]
-  : F extends [(...args: infer A) => any, ...infer Tail]
-  ? Tail extends [(arg: infer B) => any, ...any[]]
-    ? PipeArgs<Tail, [...Acc, (...args: A) => B]>
-    : Acc
-  : Acc;
+type PipeArgs<
+    F extends AnyFunc[], Acc extends AnyFunc[] = []
+> = F extends [(...args: infer A) => infer B]
+      ? [...Acc, (...args: A) => B]
+      : F extends [(...args: infer A) => any, ...infer Tail]
+        ? Tail extends [(arg: infer B) => any, ...any[]]
+          ? PipeArgs<Tail, [...Acc, (...args: A) => B]>
+          : Acc
+        : Acc;
 
-type LastFnReturnType<F extends Array<AnyFunc>, Else = never> = F extends [
-  ...any[],
-  (...arg: any) => infer R
-] ? R : Else;  
+type LastFnReturnType<
+  F extends Array<AnyFunc>, Else = never
+> = F extends [...any[], (...arg: any) => infer R]
+      ? R
+      : Else;
 
 export function pipe<F extends AnyFunc[]>(
   arg: Parameters<F[0]>[0],
